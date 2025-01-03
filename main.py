c72c962fff2cdd130d0f30ea83c1d5278c4d819b
Karan Raj Sharma <71350839+kython220282@users.noreply.github.com>
2025-01-03 20:39:57 +0530

    Update main.py
    tabs linked

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 import streamlit as st
-st.set_page_config(layout="wide")
-st.title('Performance Dashboard')
-st.write('--------')
+
+from pages.tab_one import tab_one
+from pages.tab_two import tab_two
+
+st.set_page_config(layout="wide")  # Optional: Full-width layout
+
+# Tab navigation
+tabs = st.tabs(["Tab One", "Tab Two"])
+
+with tabs[0]:
+    tab_one()
+
+with tabs[1]:
+    tab_two()
