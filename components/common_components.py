89d55f13e27f9a351624ff742a66a19b91279423
Karan Raj Sharma <71350839+kython220282@users.noreply.github.com>
2025-01-03 20:53:53 +0530

    Update common_components.py

--- components/common_components.py
+++ components/common_components.py
@@ -1,1 +1,9 @@
-#test 
+import streamlit as st
+
+def render_common_components():
+    """Render common filters used across all tabs."""
+    st.header("Filters")
+    date_range = st.date_input("Select Date Range", [])
+    filter_1 = st.selectbox("Filter 1", ["Option 1", "Option 2", "Option 3"]) #Update the Filter name and options
+    filter_2 = st.multiselect("Filter 2", ["Option A", "Option B", "Option C"]) #Update the Filter name and options
+    return date_range, filter_1, filter_2
