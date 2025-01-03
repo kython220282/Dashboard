737f925311452dd14d4f2fcaa7a69385ab25485d
Karan Raj Sharma <71350839+kython220282@users.noreply.github.com>
2025-01-03 20:48:29 +0530

    Update tab_one.py

--- pages/tab_one.py
+++ pages/tab_one.py
@@ -1,11 +1,11 @@
 import streamlit as st
+from components.common_components import render_common_components
 
 def tab_one():
-    """Content and layout for Tab One."""
     st.title("Tab One")
     col1, col2 = st.columns([1, 3])
     with col1:
         date_range, filter_1, filter_2 = render_common_components()
     with col2:
         st.write("Content specific to Tab One")
-        # Add tab-specific content here
+    
